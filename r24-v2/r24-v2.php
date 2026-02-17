b9dc907f3e94192d8f69b08247fc7774c28e6ab8
Diamondszone <120200944+Diamondszone@users.noreply.github.com>
2026-02-18 04:03:42 +0700

    Update r24-v2.php

--- r24-v2/r24-v2.php
+++ r24-v2/r24-v2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>R24ˢʰᵉˡˣ</title>
+    <title>R24ˢʰᵉˡˡ</title>
     <meta name="robots" content="noindex,nofollow">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -918,7 +918,7 @@ echo ('
                     <span class="main">
                         <span class="kanji">尺</span><span class="num">24</span>
                     </span>
-                    <span class="sup">ˢʰᵉˡˣ</span>
+                    <span class="sup">ˢʰᵉˡˡ</span>
                 </div>
             </div>
         </div>
